793dce582d31f5c3d4b8a1cc0cda8bf5d283fb13
Salonisingh044 <salosingh044@gmail.com>
2017-07-19 15:28:08 +0530

    filess added to create our foodieapp

---
+++ main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+$('#user-email').on('input',function() {
+        var email = $('#user-email').val()
+        var message = 'Welcome Back, ' + email;
+        $('.welcome-message').text(message);
+    });
+	});
